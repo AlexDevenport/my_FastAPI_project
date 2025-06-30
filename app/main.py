6d6bbf18b98fa7ea283f9b9d95be40d9d8e18001
AlexDevenport <127614739+AlexDevenport@users.noreply.github.com>
2025-06-30 17:07:18 +0500

    Work with Optional+Query

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from typing import Optional # для опциональных аргументов
+from datetime import date # для типа данных "дата"
+from fastapi import Query # для ограничения введенных значений
 
-app = FastAPI()     
+app = FastAPI()
 
-
-@app.get('/hotels/{hotel_id}') 
-def get_hotels(hotel_id: int, date_from, date_to):  
-    return hotel_id
+@app.get('/hotels')
+def get_hotels(
+	location: str,   # строка местоположения (обяз.)
+	date_from: date, # дата прибытия (обяз.)
+	date_to: date,   # дата отбытия (обяз.)
+	has_spa: Optional[bool] = None, # опциональный логический с None
+	# ограничение кол-ва звезд отеля
+	stars: Optional[int] = Query(None, ge=1, le=5)
+):
+	return location, date_from, date_to, has_spa, stars
